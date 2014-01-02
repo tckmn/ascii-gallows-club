1425c3620b217c413556dbe1d0a95027f308b92d
user2509848 <abrahamhorsch@gmail.com>
2014-01-02 10:08:56 -0600

    Art

--- hangman.py
+++ hangman.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
+
 import random # for choosing a word
 import os # for finding files in wordlists directory
 import textwrap # for ASCII art dedentation
 import time # for sleeping at the end
 
+
 # grab all the filenames
 categories = [f.split('.')[0] for f in os.listdir('wordlists')]
+
 
 def print_ASCII(ascii_str):
     "Print ASCII art."
     print(textwrap.dedent(ascii_str))
+
 
 def header():
     "Print the header."
@@ -19,18 +23,18 @@ def header():
         | Welcome to Hangman!!! |
         \\=======================/
     """)
-    print_ASCII("""
 
-    """)
 
 def choose_category():
     "Request the user to input a category."
+    print('\nCategories:')
     for cat in categories:  # print the categories
         print(cat)
-    choice = input('Please enter a category name: ').lower()
+    choice = input('\nPlease enter a category name: ').lower()
     while choice not in categories:  # loop until user enters a valid category
         choice = input('Invalid category. Try again: ')
     return choice
+
 
 def get_word(category):
     "Retrieve a random word from a category."
@@ -38,15 +42,18 @@ def get_word(category):
         words = f.readlines()
     return random.choice(words).rstrip('\n').lower().strip()
 
+
 def get_guess(guessed):
     "Get the user to guess a letter."
-    guess = input('Guess: ')
+    guess = input('\nGuess: ')
     while guess in guessed or len(guess) != 1: # guess has to be a single letter
         guess = input('You already guessed that. Try again: ' if guess in guessed else 'You must guess a single letter: ')
     return guess
 
+
 def draw_board(bad_guesses, word):
     "Draw the gallows."
+
 
     # ASCII art data
     hangs = list(map(lambda x: textwrap.dedent(x).strip('\n'), ["""
@@ -87,21 +94,25 @@ def draw_board(bad_guesses, word):
         """
     ]))
 
+
     # This is always drawn
     print_ASCII("""
         ______________
         | /          |
         |/           |""")
 
+
     # different level of hang-ness
     print(hangs[len(bad_guesses)])
+
 
     # this is also always drawn
     print_ASCII(("""
         |
         |
         | %s
-        |________________|""" % ' '.join(map(lambda s: '[%s]' % s, bad_guesses))).strip('\n'))
+        |________________|""" % ' '.join(bad_guesses)).strip('\n'))
+
 
 def hangman():  # main function
     """
@@ -109,11 +120,15 @@ def hangman():  # main function
     Returns True if the user won, False otherwise.
     """
 
+
     word = get_word(choose_category())
-    print('The word is %s (for debugging purposes)' % word)
+    #print('The word is %s (for debugging purposes)' % word)
+    print('')
+
 
     # the lines showing the word so far
     guess = ['_' if char != ' ' else ' ' for char in word]
+
 
     # already guessed and wrong guesses
     guessed = []
@@ -122,44 +137,50 @@ def hangman():  # main function
         # print letters so far
         print(' '.join(guess))
 
+
         # request input
         letter = get_guess(guessed)
         guessed.append(letter)
+
 
         # check if letter is correct or not
         if letter in word:
             # update chars guessed so far
             guess = [letter if char == letter else guess_char for char, guess_char in zip(word, guess)]
 
+
             # did they win?
             if ''.join(guess) == word:
                 print('Congratulations, you guessed the word: %s!' % word)
-                return True # TODO add replay functionality?
+                return True # TODO add replay functionality - done.
         else:
             bad_guesses.append(letter)
             print('%s is not in the word.' % letter)
 
+
         # add a little separation
-        print()
         draw_board(bad_guesses, word)
+        print()
+
 
     # if program reaches here, user failed
     print('You couldn\'t guess the word. It was %s.' % word)
     return False
 
+def play_again():
+    return input('Play again? (y/n): ').lower() == 'y'
+    
 if __name__ == '__main__':
+    
     header()
     wins, losses = 0, 0
+    
     if hangman(): wins += 1
     else: losses += 1
-    while True:
-        print('You have won %i times and lost %i times.' % (wins, losses))
-        again = input('Play again? (y/n): ').lower() == 'y'
-        print() # separation
-        if again:
-            if hangman(): wins += 1
-            else: losses += 1
-        else:
-            break
+    
+    while play_again():
+        if hangman(): wins += 1
+        else: losses += 1
+        
     print('Goodbye!')
     time.sleep(2)
