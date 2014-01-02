e70f1c5ee2216148f50d43ae8f7b0a62df6c9823
Victor Wu <vicciewu@gmail.com>
2014-01-02 12:16:14 +1100

    Added food list

--- hangman.py
+++ hangman.py
@@ -1,2 +1,8 @@
-print 'This game is so fun right'
-print 'You won'
+#!/usr/bin/env python3
+
+def hangman():
+    print('This game is so fun right')
+    print('You won')
+
+if __name__ == '__main__':
+    hangman()
